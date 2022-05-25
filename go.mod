0fdc133ae564dc8318c7995b6d19d66191cf19ca
Sanjay Singh <sanjaythakur1729@gmail.com>
2022-05-25 11:59:09 +0530

    File Uplaod

--- go.mod
+++ go.mod
@@ -9,10 +9,15 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute v1.6.1 // indirect
 	github.com/antzucaro/matchr v0.0.0-20210222213004-b04723ef80f0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -30,11 +35,19 @@ require (
 	github.com/spf13/viper v1.11.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/urfave/cli/v2 v2.8.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.80.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220524023933-508584e28198 // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,127 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
 	"helloWorld/config"
 	"helloWorld/database"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 )
+
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func fileUploadGoogleDrive(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	//file := &drive.File{Name: "Makefile", MimeType: "text/plain"}
+	res := srv.Files.Export("Make", "text/plain")
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println(res)
+
+	//file := &drive.File{Name: "Makefile", MimeType: "text/plain"}
+	//res, err := srv.Files.Create(file).Do()
+	//if err != nil {
+	//	log.Fatalf("Unable to retrieve files: %v", err)
+	//}
+	//fmt.Println(res)
+
+	//res, err := srv.Files.List().PageSize(10).
+	//	Fields("nextPageToken, files(id, name)").Do()
+	//fmt.Println("Files:", res)
+	//if len(res.Files) == 0 {
+	//	fmt.Println("No files found.")
+	//} else {
+	//	for _, i := range res.Files {
+	//		fmt.Printf("%s (%s)\n", i.Name, i.Id)
+	//	}
+	//}
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "root",
 	Short: "This is the root command",
 	Long:  "It is the root command of cobra",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Root command is here bro.")
+		fmt.Println("Root command is here.")
 	},
 }
 
@@ -81,7 +181,7 @@ func Execute() {
 }
 
 type User struct {
-	name string
+	Name string `json:"name"`
 }
 
 func getAllUsers(db *sql.DB) []string {
@@ -93,35 +193,99 @@ func getAllUsers(db *sql.DB) []string {
 	}
 	user := new(User)
 	for rows.Next() {
-		rows.Scan(&user.name)
-		users = append(users, user.name)
+		rows.Scan(&user.Name)
+		users = append(users, user.Name)
 	}
 	return users
 }
 
-func httpServer() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This Is Our Golang Server")
-	})
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		users := getAllUsers(database.Get())
-		json.NewEncoder(w).Encode(users)
-	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func insertIntoDatabase(db *sql.DB, userName string) {
-	sqlStatement := `INSERT INTO users (name) VALUES ($1)`
-	_, err := db.Exec(sqlStatement, userName)
+func insertUser(db *sql.DB, name string) {
+	sqlStatement := `INSERT INTO users (name) VALUES ($1);`
+	_, err := db.Exec(sqlStatement, name)
 	if err != nil {
 		fmt.Println("Error In Database Insert", err)
 	}
 }
+
+func deleteUser(db *sql.DB, username string) {
+	fmt.Println("User Name", username)
+	sqlStatement := `DELETE FROM users WHERE name=($1);`
+	_, err := db.Exec(sqlStatement, username)
+	if err != nil {
+		fmt.Println("Error In Database Delete", err)
+	}
+}
+
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	var param = mux.Vars(r)
+	if r.Method == "GET" {
+		users := getAllUsers(database.Get())
+		json.NewEncoder(w).Encode(users)
+		fmt.Println("Get Users Method")
+	} else if r.Method == "POST" {
+		user := new(User)
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			fmt.Println("Decode Error")
+		}
+		fmt.Println("Request Body", user.Name)
+		insertUser(database.Get(), user.Name)
+		w.Write([]byte("User Added Successfully"))
+	} else if r.Method == "DELETE" {
+		fmt.Println("Delete", param["var"])
+		deleteUser(database.Get(), param["var"])
+		w.Write([]byte("User Deleted Successfully"))
+	}
+}
+
+func fileUpload(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "File Upload")
+	file, handler, err := r.FormFile("myfile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %v\n", handler.Filename)
+	fmt.Printf("File Size: %v\n", handler.Size)
+
+	res := strings.Split(handler.Filename, ".")
+	typeOfFile := res[len(res)-1]
+	fileName := fmt.Sprintf("upload-*.%s", typeOfFile)
+	tempFile, err := ioutil.TempFile("images", fileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func httpServer() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This Is Our Golang Server")
+	})
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	r.HandleFunc("/upload", fileUpload)
+	r.HandleFunc("/upload-google", fileUploadGoogleDrive)
+	r.HandleFunc("/users/{var}", handleUser)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func Add(x, y int) (res int) {
 	return x + y
 }
@@ -144,7 +308,6 @@ func main() {
 	Execute()
 	getAllUsers(database.Get())
 	config.InitializeLogger()
-	insertIntoDatabase(database.Get(), "Sanjay Singh")
 	httpServer()
 	database.CloseDatabase()
 }
